9d20d0b3c5518ba51bdf471027ea28e2ede182ec
VinTran <tranquillo.vinnie@gmail.com>
2019-02-17 12:39:00 -0500

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// Array to store querstion objects 
+
+
+// variables to counts correct answers and incorrect 
+
+
+// for loop to display questions with timeout and interval 
+
+
+// if statements to check the correct answer and move on to next question 
+
+// display user score 
