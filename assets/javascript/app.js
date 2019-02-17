3913fd1b547808ec18c74bda399b4e8a0065dd4a
VinTran <tranquillo.vinnie@gmail.com>
2019-02-17 14:47:40 -0500

    Progress

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,23 @@
 // Array to store querstion objects 
 var questionData = []
 
-var allQuestions = {
-question1:{
+var question1= {
     content: "Which is the only American Football team to go a whole season undefeated, including the Super Bowl? Miami Dolphins",
-    answers:{answer1: "Pittsburgh Steelers", answer2: "Arizona Cardinals", answer3: "Miami Dolphins", answer4: "Chicago Bears"}
-},
+    answer1: "Pittsburgh Steelers", answer2: "Arizona Cardinals", answer3: "Miami Dolphins", answer4: "Chicago Bears"
+};
 
 
-question2: {
+    var question2 =  {
     content: "How many NBA championships did Michael Jordan win with the Chicago Bulls?",
-    answers:{answer1: "3", answer2: "6", answer3: "4", answer4: "12,000,000"}
-},
+    answer1: "3", answer2: "6", answer3: "4", answer4: "12,000,000"
+};
 
 
-question3: {
+    var question3 =  {
     content: "Which NFL team appeared in four consecutive Super Bowls from 1991 - 1994 and lost them all?",
-    answers:{answer1: "Tampa Bay Buccaneers", answer2: "Jacksonville Jaguars", answer3: "Philadelphia Eagles", answer4: "Buffalo Bills"}
-}
+    answer1: "Tampa Bay Buccaneers", answer2: "Jacksonville Jaguars", answer3: "Philadelphia Eagles", answer4: "Buffalo Bills"
+};
 
-}
 
 // variables to counts correct answers and incorrect 
 var correctAnswers = 0;
@@ -32,12 +30,21 @@ startGame();
 
 function startGame(){
 //push questions to array
-questionData.push(allQuestions);
+questionData.push(question1, question2, question3);
 
 console.log(questionData);
 
+/*chosenQuestion = questionData[Math.floor(Math.random() * questionData.length)];
+$("#question").text(chosenQuestion);
+console.log(chosenQuestion);*/
+
 for (var i = 0; i < questionData.length; i++){
-    console.log(allQuestions.question1);
+    $("#question").text(questionData[i].content);
+    $("#ans1").text(questionData[i].answer1);
+    $("#ans2").text(questionData[i].answer2);
+    $("#ans3").text(questionData[i].answer3);
+    $("#ans4").text(questionData[i].answer4);
+
 }
 
 }
